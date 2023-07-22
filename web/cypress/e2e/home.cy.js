caab3fb979c826af262559611b45eb886b816875
Fernando Papito <fernandorcks@gmail.com>
2023-07-22 19:45:29 -0300

    ajustando a validação no primeiro teste

--- web/cypress/e2e/home.cy.js
+++ web/cypress/e2e/home.cy.js
@@ -3,6 +3,6 @@ describe('home', () => {
     cy.visit('http://localhost:3000')
 
     cy.title()
-      .should('eq', 'Health eXperience | Exclusivo para treinamentos na QAx')
+      .should('equal', 'Health eXperience | Exclusivo para treinamentos na QAx')
   })
 })
