3b01bcc2bb5c2358ab769a117596c995a710c2b0
Fernando Papito <fernandorcks@gmail.com>
2023-07-29 13:42:52 -0300

    Custom Commands

--- web/cypress/e2e/enrollments.cy.js
+++ web/cypress/e2e/enrollments.cy.js
@@ -15,55 +15,28 @@ describe('matriculas', ()=> {
         enrollsPage.navbar.goToEnrolls()
         enrollsPage.goToForm()
         enrollsPage.selectItem('student', dataTest.student.name)
-        enrollsPage.selectItem('plan', dataTest.plan)
+        enrollsPage.selectItem('plan', dataTest.plan.name)
         enrollsPage.fillCard(dataTest.student)
         enrollsPage.submit()
 
         enrollsPage.popup.haveText('Matrícula cadastrada com sucesso.')
     })
 
-    it.only('não deve criar matricula duplicada', ()=> {
+    it('não deve criar matricula duplicada', ()=> {
         const dataTest = data.duplicate
 
         cy.task('resetStudent', dataTest.student)
-        cy.task('selectStudentId', dataTest.student.email)
-            .then(result => {
-   
-                cy.request({
-                    url: 'http://localhost:3333/sessions',
-                    method: 'POST',
-                    body: {
-                        email: "admin@healthxp.com",
-                        password: "xperience"
-                    }
-                }).then(response=> {
-                    cy.log(response.body.token)
+        cy.createEnroll(dataTest)
 
-                    const payload = {
-                        student_id: result.success.rows[0].id,
-                        plan_id: dataTest.plan.id,
-                        credit_card: "4242"
-                    }
+        cy.adminLogin()
 
-                    cy.request({
-                        url: 'http://localhost:3333/enrollments',
-                        method: 'POST',
-                        body: payload,
-                        headers: {
-                            Authorization: 'Bearer ' + response.body.token
-                        }
-                    }).then(response => {
-                        expect(response.status).to.eq(201)
-                    })
+        enrollsPage.navbar.goToEnrolls()
+        enrollsPage.goToForm()
+        enrollsPage.selectItem('student', dataTest.student.name)
+        enrollsPage.selectItem('plan', dataTest.plan.name)
+        enrollsPage.fillCard(dataTest.student)
+        enrollsPage.submit()
 
-                })
-
-
-
-            })
-
-        
-
-
+        enrollsPage.popup.haveText('O aluno já possui matrícula cadastrada!')
     })
 })
--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -29,9 +29,43 @@ import users from '../fixtures/users.json'
 import loginPage from './pages/LoginPage'
 import studentPage from './pages/StudentPage'
 
-Cypress.Commands.add('adminLogin', ()=> {
+Cypress.Commands.add('adminLogin', () => {
     const user = users.admin
 
     loginPage.doLogin(user)
     studentPage.navbar.userLoggedIn(user.name)
+})
+
+Cypress.Commands.add('createEnroll', (dataTest) => {
+    cy.task('selectStudentId', dataTest.student.email)
+        .then(result => {
+            cy.request({
+                url: 'http://localhost:3333/sessions',
+                method: 'POST',
+                body: {
+                    email: users.admin.email,
+                    password: users.admin.password
+                }
+            }).then(response => {
+                cy.log(response.body.token)
+
+                const payload = {
+                    student_id: result.success.rows[0].id,
+                    plan_id: dataTest.plan.id,
+                    credit_card: "4242"
+                }
+
+                cy.request({
+                    url: 'http://localhost:3333/enrollments',
+                    method: 'POST',
+                    body: payload,
+                    headers: {
+                        Authorization: 'Bearer ' + response.body.token
+                    }
+                }).then(response => {
+                    expect(response.status).to.eq(201)
+                })
+
+            })
+        })
 })
